e4cb365f2df095c8c444c7ac8a1c4a221f712432
craig <craig.freiwald@gmail.com>
2022-06-21 17:51:11 -0400

    Added comments for review.

--- weather.py
+++ weather.py
@@ -1,23 +1,35 @@
+# Project: Adventure Weather
+# Purpose Details: Group 3 Capstone Project
+# Course: IST 440W
+# Authors: Craig Freiwald, Sabrina Matteoli, Zachary Huff
+# Date Developed: 14 June 2022
+# Last Date Changed: 21 June 2022
+# Revision #: 3
+
+
+# Import Flask utilities
 from flask import Flask, render_template, request, abort
-# import json to load json data to python dictionary
+# Import JSON to use json.loads() to add API return data to dictionary
 import json
-# urllib.request to make a request to api
+# Import urllib.request to make a request to api
 import urllib.request
 
+# Name to identify app when running
 app = Flask(__name__)
 
 
-# converts kelvin returned from API to Celsius
+# Converts Kelvin to Celsius
 def toCelsius(temp):
     return str(round(float(temp) - 273.16, 1))
 
 
-# converts kelvin returned from API to Fahrenheit
+# Converts Kelvin to Fahrenheit
 def toFahrenheit(temp):
     inf = round((float(temp) - 273.16), 2) * 1.8 + 32
     return str(round(inf, 0))
 
 
+# Possibly deprecated function - keeping for future use
 # # finds requested city id in cit.list.json
 # def findID(city):
 #     input_file = open('city.list.json')
@@ -28,7 +40,7 @@ def toFahrenheit(temp):
 #         city_name = {"name": None, "id": None, 'name': item['name'], 'id': item['id']}
 #         city_list.append(city_name)
 
-
+# Set routes for city request
 @app.route('/', methods=['POST', 'GET'])
 def weather():
     api_key = '66e64fc4eb7e73b64c9e5eeccfcaed4c'
@@ -38,16 +50,17 @@ def weather():
         # default city
         city = 'Miami'
 
-    # source contain json data from api
+    # API Call from OpenWeather returns JSON of City data if found 404 if not
     try:
         source = urllib.request.urlopen(
             'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key).read()
     except:
         return abort(404)
-    # converting json data to dictionary
+
+    # Convert JSON data to dictionary
     list_of_data = json.loads(source)
 
-    # data for variable list_of_data
+    # Data for loading variable list_of_data
     data = {
         "country_code": str(list_of_data['sys']['country']),
         "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
@@ -56,10 +69,13 @@ def weather():
         "pressure": str(list_of_data['main']['pressure']),
         "humidity": str(list_of_data['main']['humidity']),
         "cityname": str(city),
+        # cityid is used when findID function is active
         # "cityid": findID(city)
     }
+    # Use data list to render info in index.html
     return render_template('index.html', data=data)
 
 
+# Run application (in debug mode)
 if __name__ == '__main__':
     app.run(debug=True)
